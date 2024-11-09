8f46765afde47030e3e30b203283ff997fc40520
DeM0NChiCk <toropov.116gh@gmail.com>
2024-11-09 13:53:45 +0300

    экспериментальные изменения N2

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,7 @@ public class Main {
 }
 
 // git config --global user.name "Your name"
-// git config --global user.email "Your email@example.com"
-//
+
+
+
+// git config --global user.email "Your email@example.com
